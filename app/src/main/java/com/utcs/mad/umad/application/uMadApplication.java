35724fda76180cccf05cc094fe51e37da60d4cf7
drewromanyk <drewiswaycool@gmail.com>
2016-01-21 19:17:09 -0600

    added push notifications

--- app/src/main/java/com/utcs/mad/umad/application/uMadApplication.java
+++ app/src/main/java/com/utcs/mad/umad/application/uMadApplication.java
@@ -1,10 +1,14 @@
 package com.utcs.mad.umad.application;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.parse.Parse;
 import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParsePush;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.utcs.mad.umad.BuildConfig;
 
 /**
@@ -19,6 +23,17 @@ public class uMadApplication extends Application {
 
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, BuildConfig.PARSE_ID, BuildConfig.PARSE_CLIENT_KEY);
+
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
 
 //        ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();
